4b6e4a4d59cb1bdf7947d7c77b945aa9260b1351
Aubrey-Tsorayi <aubreytsorayi@yahoo.com>
2021-03-06 12:41:43 +0200

    add sort choice in students table

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-#uncomment the section below to create table and database
+# uncomment the section below to create table and database
 """con = sqlite3.connect('students.db')
 c = con.cursor()
 c.execute('''CREATE TABLE students (
@@ -17,113 +17,170 @@ print('Done!')
 con.commit()
 con.close()"""
 
-#adding student
-def add(stud,name,surname):
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	info = [(stud),(name),(surname)]
-	try: #to check if student ID aready taken
-		c.execute("INSERT INTO students (StudID,Name,Surname) VALUES (?,?,?)",info)
-		con.commit()
-		con.close()
-	except sqlite3.IntegrityError:
-		print("Student ID already used")
+# adding student
+def add(stud, name, surname):
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    info = [(stud), (name), (surname)]
+    try:  # to check if student ID aready taken
+        c.execute("INSERT INTO students (StudID,Name,Surname) VALUES (?,?,?)", info)
+        con.commit()
+        con.close()
+    except sqlite3.IntegrityError:
+        print("Student ID already used")
 
-#deleting student
+
+# deleting student
 def delete(stud):
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	delete = 'DELETE FROM students WHERE StudID = ?'
-	choice = input("Press Y to accept or N to cancel: ")
-	if choice.upper() == 'Y' :
-		c.execute(delete,(stud,))
-		print("Student has been deleted")
-		con.commit()
-		con.close()
-	elif choice.upper() == 'N' :
-		print("Operation canceled")
-		pass
-	else:
-		print("Option chosen is invaild, Please Try agian.")
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    delete = "DELETE FROM students WHERE StudID = ?"
+    choice = input("Press Y to accept or N to cancel: ")
+    if choice.upper() == "Y":
+        c.execute(delete, (stud,))
+        print("Student has been deleted")
+        con.commit()
+        con.close()
+    elif choice.upper() == "N":
+        print("Operation canceled")
+        pass
+    else:
+        print("Option chosen is invaild, Please Try agian.")
 
-#searching for a student
+
+# searching for a student
 def search():
-	stud = input("Enter Student number: ")
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	find ="SELECT * FROM students WHERE StudID = '{}'".format(stud)
-	c.execute(find)
-	print("STUDID","\tNAME","\tSNAME","\tMATHS","\tENGLI","\tGENP","\tHIST")
-	print("-----------------------------------------------------------")
-	print(c.fetchone())
-	con.commit()
-	con.close()
+    stud = input("Enter Student number: ")
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    find = "SELECT * FROM students WHERE StudID = '{}'".format(stud)
+    c.execute(find)
+    print("STUDID", "\tNAME", "\tSNAME", "\tMATHS", "\tENGLI", "\tGENP", "\tHIST")
+    print("-----------------------------------------------------------")
+    print(c.fetchone())
+    con.commit()
+    con.close()
 
-#updating maths mark
+
+# updating maths mark
 def math():
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	stud = input("Enter Student number: ")
-	math = eval(input("Enter Math Percentage: "))
-	if math>0 and math<100:
-		c.execute("UPDATE students SET Math =?  WHERE StudID =?",(math,stud))
-		print("Mark has been updated")
-	else:
-		print("Mark out range")
-	con.commit()
-	con.close()
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    stud = input("Enter Student number: ")
+    math = eval(input("Enter Math Percentage: "))
+    if math > 0 and math < 100:
+        c.execute("UPDATE students SET Math =?  WHERE StudID =?", (math, stud))
+        print("Mark has been updated")
+    else:
+        print("Mark out range")
+    con.commit()
+    con.close()
 
-#updating english mark
+
+# updating english mark
 def english():
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	stud = input("Enter Student number: ")
-	english = eval(input("Enter English Percentage: "))
-	if english>0 and english<100:
-		c.execute("UPDATE students SET English =?  WHERE StudID =?",(english,stud))
-		print("Mark has been updated")
-	else:
-		print("Mark out range")
-	con.commit()
-	con.close()
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    stud = input("Enter Student number: ")
+    english = eval(input("Enter English Percentage: "))
+    if english > 0 and english < 100:
+        c.execute("UPDATE students SET English =?  WHERE StudID =?", (english, stud))
+        print("Mark has been updated")
+    else:
+        print("Mark out range")
+    con.commit()
+    con.close()
 
-#updating gp mark
+
+# updating gp mark
 def gp():
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	stud = input("Enter Student number: ")
-	gp = eval(input("Enter G.P Percentage: "))
-	if gp>0 and gp<100:
-		c.execute("UPDATE students SET GP =?  WHERE StudID =?",(gp,stud))
-		print("Mark has been updated")
-	else:
-		print("Mark out range")
-	con.commit()
-	con.close()
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    stud = input("Enter Student number: ")
+    gp = eval(input("Enter G.P Percentage: "))
+    if gp > 0 and gp < 100:
+        c.execute("UPDATE students SET GP =?  WHERE StudID =?", (gp, stud))
+        print("Mark has been updated")
+    else:
+        print("Mark out range")
+    con.commit()
+    con.close()
 
-#updating history mark
+
+# updating history mark
 def history():
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	stud = input("Enter Student number: ")
-	history = eval(input("Enter History Percentage: "))
-	if history>0 and history<100:
-		c.execute("UPDATE students SET History =?  WHERE StudID =?",(history,stud))
-		print("Mark has been updated")
-	else:
-		print("Mark out range")
-	con.commit()
-	con.close()
+    con = sqlite3.connect("students.db")
+    c = con.cursor()
+    stud = input("Enter Student number: ")
+    history = eval(input("Enter History Percentage: "))
+    if history > 0 and history < 100:
+        c.execute("UPDATE students SET History =?  WHERE StudID =?", (history, stud))
+        print("Mark has been updated")
+    else:
+        print("Mark out range")
+    con.commit()
+    con.close()
 
-#Showing the whole class
+
+# Showing the whole class
 def table():
-	con = sqlite3.connect('students.db')
-	c = con.cursor()
-	c.execute("SELECT * FROM  students")
-	results = c.fetchall()
+    print(
+        """
+        Select sort option:
 
-	print("STUDID","\tNAME","\tSNAME","\tMATHS","\tENGLI","\tGENP","\tHIST")
-	print("-----------------------------------------------------------")
-	for results in results:
-		print(results[0] , "\t" , results[1] , "\t" , results[2] , "\t" , results[3] , "\t" , results[4] , "\t" , results[5] , "\t" , results[6])
+        id: sort by studenet id
+        sur: sort by surmane
+        """
+    )
+    option = input(">> ")
 
+    if option == "id":
+
+        con = sqlite3.connect("students.db")
+        c = con.cursor()
+        c.execute("SELECT * FROM  students ORDER BY StudID ASC")
+        results = c.fetchall()
+
+        print("STUDID", "\tNAME", "\tSNAME", "\tMATHS", "\tENGLI", "\tGENP", "\tHIST")
+        print("-----------------------------------------------------------")
+        for results in results:
+            print(
+                results[0],
+                "\t",
+                results[1],
+                "\t",
+                results[2],
+                "\t",
+                results[3],
+                "\t",
+                results[4],
+                "\t",
+                results[5],
+                "\t",
+                results[6],
+            )
+
+    elif option == "sur":
+        con = sqlite3.connect("students.db")
+        c = con.cursor()
+        c.execute("SELECT * FROM  students ORDER BY Surname ASC")
+        results = c.fetchall()
+
+        print("STUDID", "\tNAME", "\tSNAME", "\tMATHS", "\tENGLI", "\tGENP", "\tHIST")
+        print("-----------------------------------------------------------")
+        for results in results:
+            print(
+                results[0],
+                "\t",
+                results[1],
+                "\t",
+                results[2],
+                "\t",
+                results[3],
+                "\t",
+                results[4],
+                "\t",
+                results[5],
+                "\t",
+                results[6],
+            )
