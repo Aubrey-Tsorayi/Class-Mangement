67247bed9a4f3f5c3d6dc795b8d961d1ebc936f8
Aubrey-Tsorayi <aubreytsorayi@yahoo.com>
2021-03-06 12:42:07 +0200

    Update

--- Class.py
+++ Class.py
@@ -1,4 +1,4 @@
-#update2
+# update2
 import database
 
 import graph
@@ -6,8 +6,10 @@ import graph
 print("Class records")
 
 while True:
+
     print("Select an option")
-	print("""
+    print(
+        """
     add: Add student
     delete: Delete student
     marks: Add marks to a student
@@ -15,66 +17,69 @@ while True:
     search: Search for a student
     graph: Graph
     quit: Quit
-    """)
+    """
+    )
 
-	choice = input(">> ")
+    choice = input(">> ")
 
-	#choice 1 adding
-	if choice.lower() == "add":
-		
-		counter = eval(input("Enter number of students to be entered: "))
+    # choice 1 adding
+    if choice.lower() == "add":
 
-		for i in range(counter):
-			stud = input("Enter Student Number: ")
-			name = input("Enter student name: ")
-			surname = input("Enter student surname: ")
-			database.add(stud,name,surname)
-	#choice 2 deleting
-	elif choice.lower() == "delete":
-		print("////////////////////////////////////////////////////////////////")
-		print("    You about to delte a record be sure before proceeding       ")
-		print("////////////////////////////////////////////////////////////////")
-		stud = input("Enter Student Number: ")
-		database.delete(stud)
-	#choice 3 updating
-	elif choice.lower() == "marks":
-		print("/// Welcome to mark update///")
-		print("///Choose an option///")
-		option = print("""
+        counter = eval(input("Enter number of students to be entered: "))
+
+        for i in range(counter):
+            stud = input("Enter Student Number: ")
+            name = input("Enter student name: ")
+            surname = input("Enter student surname: ")
+            database.add(stud, name, surname)
+    # choice 2 deleting
+    elif choice.lower() == "delete":
+        print("////////////////////////////////////////////////////////////////")
+        print("    You about to delte a record be sure before proceeding       ")
+        print("////////////////////////////////////////////////////////////////")
+        stud = input("Enter Student Number: ")
+        database.delete(stud)
+    # choice 3 updating
+    elif choice.lower() == "marks":
+        print("/// Welcome to mark update///")
+        print("///Choose an option///")
+        option = print(
+            """
 		math: adding maths marks
 		english: adding english marks
 		gp: adding gp marks
 		history: adding history marks
-		""")
+		"""
+        )
 
-		mark = input(">>: ")
-		if mark == 1:
-			database.math()
+        mark = input(">>: ")
+        if mark == 1:
+            database.math()
 
-		elif mark == 2:
-			database.english()
+        elif mark == 2:
+            database.english()
 
-		elif mark == 3:
-			database.gp()
+        elif mark == 3:
+            database.gp()
 
-		elif mark == 4:
-			database.history()
-		else:
-			print("Invalid option, try again")
+        elif mark == 4:
+            database.history()
+        else:
+            print("Invalid option, try again")
 
-	#choice 4 data presentation
-	elif choice,lower() == "students":
-		database.table()
+    # choice 4 data presentation
+    elif choice.lower() == "students":
+        database.table()
 
-	#choce 5 searching
-	elif choice.lower() == "search":
-		database.search()
+    # choce 5 searching
+    elif choice.lower() == "search":
+        database.search()
 
-	elif choice.lower() == "graph":
-		graph.graph()
+    elif choice.lower() == "graph":
+        graph.graph()
 
-	elif choice == "quit":
-		print("THANK YOU")
-		break	
-	else:
-		print("Invalid option, try again")	
+    elif choice == "quit":
+        print("THANK YOU")
+        break
+    else:
+        print("Invalid option, try again")
